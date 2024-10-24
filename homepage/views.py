d05790e0ede012b2396e8914b2feb2602868ef05
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-10-24 13:14:04 +0700

    Backup for search

--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from menu.models import Menu
 
 # Create your views here.
 def show_main(request):
+    # Mengambil 3 menu acak dari model Menu di aplikasi menu
+    random_menu = Menu.objects.order_by('?')[:3]
+    
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'menus': random_menu,
     }
-
-    return render(request, "homepage.html", context)
+    return render(request, 'homepage.html', context)
---
+++ search/views.py
@@ -0,0 +1,30 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Menu
+
+def search_menu(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        query = request.GET.get('query', '')  # Get search query
+        budget = request.GET.get('budget', '')  # Get selected budget
+        
+        # Filter menus by name
+        results = Menu.objects.all()
+        if query:
+            results = results.filter(menu__icontains=query)
+
+        # Filter by budget if provided
+        if budget:
+            results = results.filter(harga__lte=int(budget))
+        
+        data = [
+            {
+                'menu': menu.menu,
+                'harga': menu.harga,
+                'warung': menu.warung,
+                'gambar': menu.gambar,
+            }
+            for menu in results
+        ]
+        
+        return JsonResponse({'results': data})
+    return render(request, 'search.html')
