2a514d64df646b92f1631371e9077a0c58b351c3
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-10-27 21:32:55 +0700

    fixing features

--- menu/models.py
+++ menu/models.py
@@ -4,4 +4,4 @@ class Menu(models.Model):
     warung=models.CharField(max_length=100)
     menu=models.CharField(max_length=100)
     harga=models.IntegerField()
-    gambar=models.CharField(max_length=100)
+    gambar=models.CharField(max_length=1000)
