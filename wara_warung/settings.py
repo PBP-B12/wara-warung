bd9b731c6d186d8a37e319edffdded061a6508de
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-12-02 19:03:26 +0700

    test pws again

--- wara_warung/settings.py
+++ wara_warung/settings.py
@@ -150,3 +150,5 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
+CSRF_TRUSTED_ORIGINS = ['https://jeremia-rangga-warawarung.pbp.cs.ui.ac.id']
