f3ebedb39c6fad60a9bbdc4e9e6612da89bfa696
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-12-04 14:03:32 +0700

    add flutter login and register

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('auth/registerd/', views.register, name='registerd'),
+    path('auth/logind/', views.login, name='logind'),
     ]
--- wara_warung/settings.py
+++ wara_warung/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "user_dashboard",
     'ratereview',
     'corsheaders',
+    'authenticationdummy',
 ]
 
 MIDDLEWARE = [
