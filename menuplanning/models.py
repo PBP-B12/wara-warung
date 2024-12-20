4e18defd21ae012a5acdcf704606c00116798ed6
bberliana <raniaberliana96@gmail.com>
2024-12-20 17:26:33 +0700

    fix menu planning

--- menuplanning/models.py
+++ menuplanning/models.py
@@ -26,6 +26,7 @@ class ChosenMenu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     save_session = models.IntegerField(default=0)  
     budget = models.DecimalField(max_digits=10, decimal_places=2, default=100000)
+    warung_name = models.CharField(max_length=100, default="Unknown Warung") 
 
     def __str__(self):
         return f"{self.quantity} x {self.item_name} for {self.user} (Session {self.save_session})"
